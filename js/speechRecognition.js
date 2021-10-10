4904a90addc05cefcaee8719981581d6a9f32bdd
ymzEmre <dev.yilmazemre@gmail.com>
2021-10-10 16:46:37 +0300

    Add time limit speech recognition

--- js/speechRecognition.js
+++ js/speechRecognition.js
@@ -1,9 +1,8 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-// let searchBox = document.querySelector(".searchBox");
 
 const rec = new SpeechRecognition();
 
-rec.onresult = function (event) {
+rec.onresult = (event) => {
   const current = event.resultIndex;
   const transcript = event.results[current][0].transcript;
   searchBox.value = transcript;
@@ -13,15 +12,55 @@ function recoStart() {
   rec.start();
 }
 
+const checkbox = document.getElementById("ios-checkbox");
+const calc = () => {
+  if (checkbox.checked) {
+    localStorage.setItem("autoListen", checkbox.checked);
+  } else {
+    localStorage.setItem("autoListen", checkbox.checked);
+  }
+};
+
+const checked = JSON.parse(localStorage.getItem("autoListen"));
+if (checked) {
+  checkbox.checked = true;
+  rec.start();
+} else {
+  checkbox.checked = false;
+  rec.stop();
+}
+
+// function readOutLoud(mess) {
+//   const speech = new SpeechSynthesisUtterance();
+
+//   speech.text = mess + "Derece" + cityTemp.innerHTML;
+//   speech.text = cityTemp.value;
+//   speech.volume = 1;
+//   speech.rate = 1;
+//   speech.pitch = 1;
+//   console.log(cityTemp.value);
+//   window.speechSynthesis.speak(speech);
+
+//   const utterance = new SpeechSynthesisUtterance(speech.text);
+//   window.speechSynthesis.speak(utterance);
+// }
+
+const micStop = () => {
+  setTimeout(() => {
+    rec.stop();
+  }, 3000);
+};
+
 document.querySelector(".iconMicrophone").addEventListener("click", recoStart);
 
-rec.addEventListener("start", function () {
+rec.onstart = (e) => {
   document.querySelector(".iconMicrophone").classList.add("speechStart");
   document.querySelector(".iconMicrophone").classList.remove("speechEnd");
-});
+  micStop();
+};
 
-rec.addEventListener("end", function () {
+rec.onend = (e) => {
   searchBox.click();
   document.querySelector(".iconMicrophone").classList.add("speechEnd");
   document.querySelector(".iconMicrophone").classList.remove("speechStart");
-});
+};
