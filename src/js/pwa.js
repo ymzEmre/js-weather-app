f6a7f36ba19b61d2d590b2a02b3b5907ab0ebc0f
ymzEmre <dev.yilmazemre@gmail.com>
2021-10-20 03:29:13 +0300

    Update pwa.js

--- src/js/pwa.js
+++ src/js/pwa.js
@@ -1,25 +1,17 @@
-let promptEvent;
+const installButton = document.querySelector(".installSection");
+const installButton2 = document.querySelector(".searchContainer");
+
+let beforeInstallPromptEvent;
 
 window.addEventListener("beforeinstallprompt", function (e) {
   e.preventDefault();
-  promptEvent = e;
-  listenToUserAction();
+  beforeInstallPromptEvent = e;
+  installButton.style.display = "flex";
+  installButton.addEventListener("click", function () {
+    e.prompt();
+  });
+  installButton.hidden = false;
 });
-
-function listenToUserAction() {
-  const installBtn = document.querySelector(".installSection");
-  installBtn.style.display = "flex";
-
-  installBtn.addEventListener("click", presentAddToHome);
-}
-
-function presentAddToHome() {
-  promptEvent.prompt();
-  // promptEvent.userChoice.then((choice) => {
-  //   if (choice.outcome === "accepted") {
-  //     console.log("User accepted");
-  //   } else {
-  //     console.log("User dismissed");
-  //   }
-  // });
-}
+installButton.addEventListener("click", function () {
+  beforeInstallPromptEvent.prompt();
+});
