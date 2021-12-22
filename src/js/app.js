169ffc0eac7a1555b10b233288f70f0910c7b6db
ymzEmre <dev.yilmazemre@gmail.com>
2021-12-22 16:09:47 +0300

    Edit app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,27 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const weatherApiKey = "9dc471d76a73a52a9fb31b72dec034bd";
-const weatherApiUrl = "https://api.openweathermap.org/data/2.5/";
+const weatherApiKey = process.env.API_API_KEY;
+const weatherApiUrl = process.env.API_API_URL;
 
-const bodyEl = document.getElementsByTagName("body")[0];
+const bodyEl = document.getElementsByTagName('body')[0];
 
-const checkboxDegreeUnitEl = document.getElementById("checkboxUnitDegree");
-const searchBoxEl = document.querySelector(".searchBox");
-
-const checkboxSectionSpan = document.querySelectorAll(".checkboxSection p span");
+const checkboxDegreeUnitEl = document.getElementById('checkboxUnitDegree');
+const searchBoxEl = document.querySelector('.searchBox');
 
 const changeUnitDegree = () => {
   setTimeout(() => {
-    currentUnitDegree = checkboxDegreeUnitEl.checked ? "imperial" : "metric";
+    currentUnitDegree = checkboxDegreeUnitEl.checked ? 'imperial' : 'metric';
     searchBoxEl.click();
     if (checkboxDegreeUnitEl.checked) {
-      localStorage.setItem("currentUnitDegree", "imperial");
+      localStorage.setItem('currentUnitDegree', 'imperial');
     } else {
-      localStorage.setItem("currentUnitDegree", "metric");
+      localStorage.setItem('currentUnitDegree', 'metric');
     }
   }, 300);
 };
 
-const getDegreeUnitValue = localStorage.getItem("currentUnitDegree");
-if (getDegreeUnitValue == "imperial") {
+const getDegreeUnitValue = localStorage.getItem('currentUnitDegree');
+if (getDegreeUnitValue == 'imperial') {
   checkboxDegreeUnitEl.checked = true;
   changeUnitDegree();
 } else {
@@ -35,54 +33,54 @@ const setQuery = (e) => {
   getResult(searchBoxEl.value);
 };
 
-const cityResultSectionEl = document.querySelector(".cityResultSection");
+const cityResultSectionEl = document.querySelector('.cityResultSection');
 
-const cityResultInfoMainEl = document.querySelector(".cityResultInfoMain");
-const cityNameEl = document.querySelector(".cityName");
-const cityTempEl = document.querySelector(".cityTemp");
-const cityDescEl = document.querySelector(".cityDesc");
+const cityResultInfoMainEl = document.querySelector('.cityResultInfoMain');
+const cityNameEl = document.querySelector('.cityName');
+const cityTempEl = document.querySelector('.cityTemp');
+const cityDescEl = document.querySelector('.cityDesc');
 
-const cityResultInfoOtherEl = document.querySelector(".cityResultInfoOther");
-const cityIconEl = document.querySelectorAll(".cityResultInfoOther i");
-const cityMinMaxEl = document.querySelector(".cityMinMax");
-const cityHumidityEl = document.querySelector(".cityHumidity");
-const cityWindSpeedEl = document.querySelector(".cityWindSpeed");
-const citySunriseEl = document.querySelector(".citySunrise");
-const citySunsetEl = document.querySelector(".citySunset");
-const cityNotFoundEl = document.querySelector(".cityNotFound");
+const cityResultInfoOtherEl = document.querySelector('.cityResultInfoOther');
+const cityIconEl = document.querySelectorAll('.cityResultInfoOther i');
+const cityMinMaxEl = document.querySelector('.cityMinMax');
+const cityHumidityEl = document.querySelector('.cityHumidity');
+const cityWindSpeedEl = document.querySelector('.cityWindSpeed');
+const citySunriseEl = document.querySelector('.citySunrise');
+const citySunsetEl = document.querySelector('.citySunset');
+const cityNotFoundEl = document.querySelector('.cityNotFound');
 
-const checkboxSectionLabel = document.querySelectorAll(".checkboxSection p");
+const checkboxSectionLabel = document.querySelectorAll('.checkboxSection p');
 
-const installSection = document.querySelector(".installSection p");
+const installSection = document.querySelector('.installSection p');
 
-const soundClearDayTimeEl = document.getElementById("soundClearDayTime");
-const soundClearNightTimeEl = document.getElementById("soundClearNightTime");
-const soundRainyEl = document.getElementById("soundRainy");
-const soundSnowyEl = document.getElementById("soundSnowy");
-const soundMistyEl = document.getElementById("soundMisty");
-const soundThunderStormEl = document.getElementById("soundThunderStorm");
+const soundClearDayTimeEl = document.getElementById('soundClearDayTime');
+const soundClearNightTimeEl = document.getElementById('soundClearNightTime');
+const soundRainyEl = document.getElementById('soundRainy');
+const soundSnowyEl = document.getElementById('soundSnowy');
+const soundMistyEl = document.getElementById('soundMisty');
+const soundThunderStormEl = document.getElementById('soundThunderStorm');
 
 const getResult = (weatherApiCity) => {
   let query = `${weatherApiUrl}weather?q=${weatherApiCity}&appid=${weatherApiKey}&units=${currentUnitDegree}`;
 
-  cityNameEl.innerText = "";
-  cityTempEl.innerText = "";
-  cityDescEl.innerText = "";
-  cityMinMaxEl.innerText = "";
-  cityNotFoundEl.innerText = "";
-  cityHumidityEl.innerText = "";
-  citySunriseEl.innerText = "";
-  citySunsetEl.innerText = "";
-  cityWindSpeedEl.innerText = "";
+  cityNameEl.innerText = '';
+  cityTempEl.innerText = '';
+  cityDescEl.innerText = '';
+  cityMinMaxEl.innerText = '';
+  cityNotFoundEl.innerText = '';
+  cityHumidityEl.innerText = '';
+  citySunriseEl.innerText = '';
+  citySunsetEl.innerText = '';
+  cityWindSpeedEl.innerText = '';
 
   for (i = 0; i < cityIconEl.length; i++) {
-    cityIconEl[i].classList.add("displayOff");
+    cityIconEl[i].classList.add('displayOff');
   }
 
-  checkboxSectionLabel[0].classList.remove("black");
-  checkboxSectionLabel[1].classList.remove("black");
-  installSection.classList.remove("black");
-  cityResultSectionEl.classList.remove("white");
+  checkboxSectionLabel[0].classList.remove('black');
+  checkboxSectionLabel[1].classList.remove('black');
+  installSection.classList.remove('black');
+  cityResultSectionEl.classList.remove('white');
 
   fetch(query)
     .then((weather) => {
@@ -93,15 +91,15 @@ const getResult = (weatherApiCity) => {
 
     .catch((err) => {
       if (searchBoxEl.value.length < 3) {
-        cityResultSectionEl.classList.remove("cityResultSectionDisplay");
+        cityResultSectionEl.classList.remove('cityResultSectionDisplay');
       } else if (searchBoxEl.value.length > 2) {
-        cityNotFoundEl.innerText = "City not found";
-        cityResultInfoMainEl.classList.add("displayNone");
-        cityResultInfoOtherEl.classList.add("displayNone");
-        cityResultSectionEl.classList.add("cityResultSectionDisplay");
+        cityNotFoundEl.innerText = 'City not found';
+        cityResultInfoMainEl.classList.add('displayNone');
+        cityResultInfoOtherEl.classList.add('displayNone');
+        cityResultSectionEl.classList.add('cityResultSectionDisplay');
       } else {
-        cityResultInfoMainEl.classList.remove("displayNone");
-        cityResultInfoOtherEl.classList.remove("displayNone");
+        cityResultInfoMainEl.classList.remove('displayNone');
+        cityResultInfoOtherEl.classList.remove('displayNone');
       }
       bodyEl.style.backgroundImage = "url('src/assets/img/home-page.jpg')";
       soundClearDayTimeEl.pause();
@@ -114,9 +112,9 @@ const getResult = (weatherApiCity) => {
 };
 
 const displayResult = (result) => {
-  const unitDegree = checkboxDegreeUnitEl.checked ? "°F" : "°C";
+  const unitDegree = checkboxDegreeUnitEl.checked ? '°F' : '°C';
 
-  const unitWindSpeed = checkboxDegreeUnitEl.checked ? "mph" : "km/h";
+  const unitWindSpeed = checkboxDegreeUnitEl.checked ? 'mph' : 'km/h';
 
   const msToTime = (msGetValue) => {
     let getMs = new Date(msGetValue * 1000);
@@ -124,10 +122,10 @@ const displayResult = (result) => {
     let getHours = getMs.getHours();
     let getMinutes = getMs.getMinutes();
 
-    let hours = getHours > 9 ? getHours : "0" + getHours;
-    let minutes = getMinutes > 9 ? getMinutes : "0" + getMinutes;
+    let hours = getHours > 9 ? getHours : '0' + getHours;
+    let minutes = getMinutes > 9 ? getMinutes : '0' + getMinutes;
 
-    return hours + ":" + minutes;
+    return hours + ':' + minutes;
   };
 
   let cityWindSpeed = Math.round(result.wind.speed * 3.6);
@@ -141,35 +139,35 @@ const displayResult = (result) => {
   cityDescEl.innerText = result.weather[0].description;
   cityMinMaxEl.innerText = `${Math.round(result.main.temp_min)} / ${Math.round(result.main.temp_max)}`;
   cityHumidityEl.innerText = `${result.main.humidity}%`;
-  cityWindSpeedEl.innerText = cityWindSpeed + " " + unitWindSpeed;
+  cityWindSpeedEl.innerText = cityWindSpeed + ' ' + unitWindSpeed;
   citySunriseEl.innerText = msToTime(`${result.sys.sunrise}`);
   citySunsetEl.innerText = msToTime(`${result.sys.sunset}`);
 
-  checkboxSectionLabel[0].classList.add("black");
-  checkboxSectionLabel[1].classList.add("black");
-  installSection.classList.add("black");
+  checkboxSectionLabel[0].classList.add('black');
+  checkboxSectionLabel[1].classList.add('black');
+  installSection.classList.add('black');
 
-  cityResultInfoMainEl.classList.remove("displayNone");
-  cityResultInfoOtherEl.classList.remove("displayNone");
+  cityResultInfoMainEl.classList.remove('displayNone');
+  cityResultInfoOtherEl.classList.remove('displayNone');
 
   for (i = 0; i < cityIconEl.length; i++) {
-    cityIconEl[i].classList.remove("displayOff");
+    cityIconEl[i].classList.remove('displayOff');
   }
 
-  cityResultSectionEl.classList.add("cityResultSectionDisplay");
+  cityResultSectionEl.classList.add('cityResultSectionDisplay');
 
   let getDesc = result.weather[0].main;
 
   if (getDesc.length > 0) {
-    cityNotFoundEl.innerText = "";
+    cityNotFoundEl.innerText = '';
   }
 
-  let currentTime = new Date().toLocaleTimeString("en-US", { hour12: false, hour: "numeric", minute: "numeric" });
+  let currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' });
 
   let getCitySunrise = msToTime(result.sys.sunrise);
 
   switch (getDesc) {
-    case (getDesc = "Clear"):
+    case (getDesc = 'Clear'):
       if (currentTime > getCitySunrise) {
         // dayTime
         bodyEl.style.backgroundImage = "url('src/assets/weather/clear-daytime.jpg')";
@@ -180,64 +178,64 @@ const displayResult = (result) => {
         bodyEl.style.backgroundImage = "url('src/assets/weather/clear-nighttime.jpg')";
         soundClearNightTimeEl.play();
 
-        installSection.classList.remove("black");
-        checkboxSectionLabel[0].classList.remove("black");
-        checkboxSectionLabel[1].classList.remove("black");
-        cityResultSectionEl.classList.add("white");
+        installSection.classList.remove('black');
+        checkboxSectionLabel[0].classList.remove('black');
+        checkboxSectionLabel[1].classList.remove('black');
+        cityResultSectionEl.classList.add('white');
       }
 
-      let notsoundClear = document.querySelectorAll("audio:not(#soundClearDayTime):not(#soundClearNightTime)");
+      let notsoundClear = document.querySelectorAll('audio:not(#soundClearDayTime):not(#soundClearNightTime)');
       for (let i = 0; i < notsoundClear.length; i++) {
         notsoundClear[i].pause();
       }
       break;
 
-    case (getDesc = "Clouds"):
+    case (getDesc = 'Clouds'):
       bodyEl.style.backgroundImage = "url('src/assets/weather/cloudy.jpg')";
       soundMistyEl.play();
 
-      let notsoundMisty = document.querySelectorAll("audio:not(#soundMisty)");
+      let notsoundMisty = document.querySelectorAll('audio:not(#soundMisty)');
       for (let i = 0; i < notsoundMisty.length; i++) {
         notsoundMisty[i].pause();
       }
       break;
 
-    case (getDesc = "Rain"):
+    case (getDesc = 'Rain'):
       bodyEl.style.backgroundImage = "url('src/assets/weather/rainy.jpg')";
       soundRainyEl.play();
 
-      let notsoundRainyEl = document.querySelectorAll("audio:not(#soundRainy)");
+      let notsoundRainyEl = document.querySelectorAll('audio:not(#soundRainy)');
       for (let i = 0; i < notsoundRainyEl.length; i++) {
         notsoundRainyEl[i].pause();
       }
       break;
 
-    case (getDesc = "Snow"):
+    case (getDesc = 'Snow'):
       bodyEl.style.backgroundImage = "url('src/assets/weather/snowy.jpg')";
       soundSnowyEl.play();
 
-      let notsoundSnowyEl = document.querySelectorAll("audio:not(#soundSnowy)");
+      let notsoundSnowyEl = document.querySelectorAll('audio:not(#soundSnowy)');
       for (let i = 0; i < notsoundSnowyEl.length; i++) {
         notsoundSnowyEl[i].pause();
       }
       break;
 
-    case (getDesc = "Mist"):
+    case (getDesc = 'Mist'):
       bodyEl.style.backgroundImage = "url('src/assets/weather/misty.jpg')";
       soundMistyEl.play();
 
-      let notsoundMistyEl = document.querySelectorAll("audio:not(#soundMisty)");
+      let notsoundMistyEl = document.querySelectorAll('audio:not(#soundMisty)');
       for (let i = 0; i < notsoundMistyEl.length; i++) {
         notsoundMistyEl[i].pause();
       }
 
       break;
 
-    case (getDesc = "Thunderstorm"):
+    case (getDesc = 'Thunderstorm'):
       bodyEl.style.backgroundImage = "url('src/assets/weather/thunderstorm.jpg')";
       soundThunderStormEl.play();
 
-      let notsoundThunderStormEl = document.querySelectorAll("audio:not(#soundThunderStorm)");
+      let notsoundThunderStormEl = document.querySelectorAll('audio:not(#soundThunderStorm)');
       for (let i = 0; i < notsoundThunderStormEl.length; i++) {
         notsoundThunderStormEl[i].pause();
       }
@@ -246,6 +244,6 @@ const displayResult = (result) => {
   }
 };
 
-"click keyup".split(" ").forEach(function (e) {
+'click keyup'.split(' ').forEach(function (e) {
   searchBoxEl.addEventListener(e, setQuery, false);
 });
